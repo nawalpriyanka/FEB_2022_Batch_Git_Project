0c4654dc953c98ded3b0be6f0cc920d4fa204c66
Priyanka Nawal <priyankanawal1990@gmail.com>
2022-06-27 09:47:31 +0530

    Initial commit

---
+++ src/test/java/com/tests/AppiumTest.java
@@ -0,0 +1,21 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void openBrowser()
+	{
+		System.out.println("Opening Browser");
+	}
+	
+	
+	@Test
+	public void closeBrowser()
+	{
+		System.out.println("Closing Browser");
+	}
+	
+
+}
---
+++ src/test/java/com/tests/SeleniumTest.java
@@ -0,0 +1,25 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void doLogin()
+	{
+		System.out.println("Executing Login Test");
+	}
+	
+	@Test
+	public void userRegistration()
+	{
+		System.out.println("Registration User");
+	}
+	
+	
+	
+	
+	
+	
+}
